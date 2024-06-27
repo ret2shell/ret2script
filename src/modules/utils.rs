f278afa3d58c91843a2dceaade6f14f1785cf4dc
Reverier-Xu <reverier.xu@woooo.tech>
2024-06-28 00:49:16 +0800

    :construction: update audit module
    Co-Authored-By: hypnoticss <3550653512@qq.com>

--- src/modules/utils.rs
+++ src/modules/utils.rs
@@ -1,7 +1,9 @@
 use rune::{ContextError, Module};
 
 /// Construct the `ret2api::utils` module.
+#[rune::module(::ret2api::utils)]
 pub fn module(_stdio: bool) -> Result<Module, ContextError> {
-  let module = Module::with_crate("ret2api::utils")?;
-  Ok(module)
+    let module = Module::from_meta(self::module_meta)?;
+    Ok(module)
 }
+
