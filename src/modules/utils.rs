f0d90e62bd1eb10468834d8d3634d918c5c2beb3
Reverier-Xu <reverier.xu@woooo.tech>
2024-06-28 03:22:24 +0800

    :construction: finish cli

--- src/modules/utils.rs
+++
@@ -1,8 +0,0 @@
-use rune::{ContextError, Module};
-
-/// Construct the `ret2api::utils` module.
-#[rune::module(::ret2api::utils)]
-pub fn module(_stdio: bool) -> Result<Module, ContextError> {
-    let module = Module::from_meta(self::module_meta)?;
-    Ok(module)
-}
