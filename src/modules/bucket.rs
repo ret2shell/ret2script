ffee6d435407e38f7f7a140f1ca82205905af037
Reverier-Xu <reverier.xu@woooo.tech>
2024-06-28 00:49:16 +0800

    :construction: update audit module

--- src/modules/bucket.rs
+++ src/modules/bucket.rs
@@ -1,7 +1,10 @@
 use rune::{ContextError, Module};
 
 /// Construct the `ret2api::bucket` module.
+#[rune::module(::ret2api::bucket)]
 pub fn module(_stdio: bool) -> Result<Module, ContextError> {
-  let module = Module::with_crate("ret2api::bucket")?;
-  Ok(module)
+    let module = Module::from_meta(self::module_meta)?;
+
+    Ok(module)
 }
+
--- src/modules/crypto.rs
+++ src/modules/crypto.rs
@@ -1,7 +1,8 @@
 use rune::{ContextError, Module};
 
 /// Construct the `ret2api::crypto` module.
+#[rune::module(::ret2api::crypto)]
 pub fn module(_stdio: bool) -> Result<Module, ContextError> {
-  let module = Module::with_crate("ret2api::crypto")?;
-  Ok(module)
+    let module = Module::from_meta(self::module_meta)?;
+    Ok(module)
 }
