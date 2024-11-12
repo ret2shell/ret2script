b04fda8e07ee5f09a0f1e8cc9d20e8f174c2bc99
Reverier-Xu <reverier.xu@woooo.tech>
2024-11-13 02:23:55 +0800

    :recycle: reformat code

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,20 @@ use clap::Parser;
 use colored::Colorize;
 use ret2script::modules::bucket::Bucket;
 use rune::{
-    alloc,
-    runtime::Object,
-    termcolor::{ColorChoice, StandardStream},
-    Context, Diagnostics, Source, Sources, Value, Vm,
+  alloc,
+  runtime::Object,
+  termcolor::{ColorChoice, StandardStream},
+  Context, Diagnostics, Source, Sources, Value, Vm,
 };
 
 #[derive(Parser, Debug)]
 #[command(name = "ret2script")]
 #[command(bin_name = "ret2script")]
 #[command(
-    author = "Reverier-Xu <reverier.xu@woooo.tech>",
-    version,
-    about = "Script checker for Ret 2 Shell Challenge API Platform",
-    long_about = r#"
+  author = "Reverier-Xu <reverier.xu@woooo.tech>",
+  version,
+  about = "Script checker for Ret 2 Shell Challenge API Platform",
+  long_about = r#"
 Script checker for Ret 2 Shell Challenge API Platform
 
 THE CONTENTS OF THIS PROJECT ARE PROPRIETARY AND CONFIDENTIAL.
@@ -28,163 +28,163 @@ If you have any problems, please contact tech support <support@ret.sh.cn>.
 "#
 )]
 enum Commands {
-    /// run `check()` function in your script.
-    Check {
-        /// script path
-        script: String,
-        #[clap(short, long)]
-        flag: String,
-    },
-    /// run `environ()` function in your script.
-    Environ {
-        /// script path
-        script: String,
-    },
+  /// run `check()` function in your script.
+  Check {
+    /// script path
+    script: String,
+    #[clap(short, long)]
+    flag: String,
+  },
+  /// run `environ()` function in your script.
+  Environ {
+    /// script path
+    script: String,
+  },
 }
 
 #[tokio::main]
 async fn main() {
-    let args = Commands::parse();
-    match args {
-        Commands::Check { script, flag } => check(&script, &flag).await.expect("error encountered"),
-        Commands::Environ { script } => environ(&script).await.expect("error encountered"),
-    }
+  let args = Commands::parse();
+  match args {
+    Commands::Check { script, flag } => check(&script, &flag).await.expect("error encountered"),
+    Commands::Environ { script } => environ(&script).await.expect("error encountered"),
+  }
 }
 
 fn compile_source(script: impl AsRef<Path>) -> anyhow::Result<Vm> {
-    let mut context = Context::with_default_modules()?;
-    context.install(rune_modules::http::module(true)?)?;
-    context.install(rune_modules::json::module(true)?)?;
-    context.install(rune_modules::toml::module(true)?)?;
-    context.install(rune_modules::process::module(true)?)?;
-    context.install(ret2script::modules::crypto::module(true)?)?;
-    context.install(ret2script::modules::bucket::module(true)?)?;
-    context.install(ret2script::modules::audit::module(true)?)?;
-    context.install(ret2script::modules::utils::module(true)?)?;
-    context.install(ret2script::modules::regex::module(true)?)?;
+  let mut context = Context::with_default_modules()?;
+  context.install(rune_modules::http::module(true)?)?;
+  context.install(rune_modules::json::module(true)?)?;
+  context.install(rune_modules::toml::module(true)?)?;
+  context.install(rune_modules::process::module(true)?)?;
+  context.install(ret2script::modules::crypto::module(true)?)?;
+  context.install(ret2script::modules::bucket::module(true)?)?;
+  context.install(ret2script::modules::audit::module(true)?)?;
+  context.install(ret2script::modules::utils::module(true)?)?;
+  context.install(ret2script::modules::regex::module(true)?)?;
 
-    let mut sources = Sources::new();
-    let mut diagnostics = Diagnostics::new();
-    sources.insert(Source::from_path(script.as_ref())?)?;
-    let unit = rune::prepare(&mut sources)
-        .with_context(&context)
-        .with_diagnostics(&mut diagnostics)
-        .build();
-    if !diagnostics.is_empty() {
-        let mut writer = StandardStream::stderr(ColorChoice::Always);
-        diagnostics.emit(&mut writer, &sources)?;
-    }
-    let unit = unit?;
-    let runtime = context.runtime()?;
-    Ok(Vm::new(Arc::new(runtime), Arc::new(unit)))
+  let mut sources = Sources::new();
+  let mut diagnostics = Diagnostics::new();
+  sources.insert(Source::from_path(script.as_ref())?)?;
+  let unit = rune::prepare(&mut sources)
+    .with_context(&context)
+    .with_diagnostics(&mut diagnostics)
+    .build();
+  if !diagnostics.is_empty() {
+    let mut writer = StandardStream::stderr(ColorChoice::Always);
+    diagnostics.emit(&mut writer, &sources)?;
+  }
+  let unit = unit?;
+  let runtime = context.runtime()?;
+  Ok(Vm::new(Arc::new(runtime), Arc::new(unit)))
 }
 
 async fn check(script: impl AsRef<Path>, flag: impl AsRef<str>) -> anyhow::Result<()> {
-    let cwd = current_dir()?;
-    let mut vm = compile_source(script)?;
-    let mut user = Object::new();
-    user.insert(alloc::String::try_from("id")?, rune::to_value(3307)?)?;
-    user.insert(
-        alloc::String::try_from("account")?,
-        rune::to_value("p1ay3r")?,
-    )?;
-    user.insert(
-        alloc::String::try_from("institute_id")?,
-        rune::to_value(1106)?,
-    )?;
-    println!("{}\t\t= {user:?}", "User".blue());
-    let mut team = Object::new();
-    team.insert(alloc::String::try_from("id")?, rune::to_value(114514)?)?;
-    team.insert(alloc::String::try_from("name")?, rune::to_value("te4m")?)?;
-    team.insert(
-        alloc::String::try_from("institute_id")?,
-        rune::to_value(1106)?,
-    )?;
-    println!("{}\t\t= {team:?}", "Team".green());
-    let mut submission = Object::new();
-    submission.insert(alloc::String::try_from("id")?, rune::to_value(1919)?)?;
-    submission.insert(alloc::String::try_from("user_id")?, rune::to_value(3307)?)?;
-    submission.insert(alloc::String::try_from("team_id")?, rune::to_value(114)?)?;
-    submission.insert(
-        alloc::String::try_from("challenge_id")?,
-        rune::to_value(810)?,
-    )?;
-    submission.insert(
-        alloc::String::try_from("content")?,
-        rune::to_value(flag.as_ref())?,
-    )?;
-    println!("{}\t= {submission:?}", "Submission".yellow());
-    let bucket = Bucket::try_new(&cwd)?;
-    let output = vm.call(["check"], (bucket, user, team, submission));
-    if output.is_err() {
-        println!(
-            "{}: {output:?}",
-            "Script ended with runtime error".red().bold()
-        );
-        return Ok(());
-    }
-    let output = output?;
+  let cwd = current_dir()?;
+  let mut vm = compile_source(script)?;
+  let mut user = Object::new();
+  user.insert(alloc::String::try_from("id")?, rune::to_value(3307)?)?;
+  user.insert(
+    alloc::String::try_from("account")?,
+    rune::to_value("p1ay3r")?,
+  )?;
+  user.insert(
+    alloc::String::try_from("institute_id")?,
+    rune::to_value(1106)?,
+  )?;
+  println!("{}\t\t= {user:?}", "User".blue());
+  let mut team = Object::new();
+  team.insert(alloc::String::try_from("id")?, rune::to_value(114514)?)?;
+  team.insert(alloc::String::try_from("name")?, rune::to_value("te4m")?)?;
+  team.insert(
+    alloc::String::try_from("institute_id")?,
+    rune::to_value(1106)?,
+  )?;
+  println!("{}\t\t= {team:?}", "Team".green());
+  let mut submission = Object::new();
+  submission.insert(alloc::String::try_from("id")?, rune::to_value(1919)?)?;
+  submission.insert(alloc::String::try_from("user_id")?, rune::to_value(3307)?)?;
+  submission.insert(alloc::String::try_from("team_id")?, rune::to_value(114)?)?;
+  submission.insert(
+    alloc::String::try_from("challenge_id")?,
+    rune::to_value(810)?,
+  )?;
+  submission.insert(
+    alloc::String::try_from("content")?,
+    rune::to_value(flag.as_ref())?,
+  )?;
+  println!("{}\t= {submission:?}", "Submission".yellow());
+  let bucket = Bucket::try_new(&cwd)?;
+  let output = vm.call(["check"], (bucket, user, team, submission));
+  if output.is_err() {
     println!(
-        "{}",
-        "---------------------------------------------".dimmed()
+      "{}: {output:?}",
+      "Script ended with runtime error".red().bold()
     );
-    let output: Result<(bool, String, Option<Object>), Value> = rune::from_value(output)?;
-    if let Ok((result, message, audit)) = output {
-        println!(
-            "{}\n\t{}\t\t= {result}\n\t{}\t\t= {message}\n\t{}\t\t= {audit:?}",
-            "Result".green(),
-            "Correct".blue(),
-            "Message".blue(),
-            "Audit".blue()
-        );
-    } else {
-        println!(
-            "{}",
-            "Script returned early from '?' operators.".red().bold()
-        );
-    }
-    Ok(())
+    return Ok(());
+  }
+  let output = output?;
+  println!(
+    "{}",
+    "---------------------------------------------".dimmed()
+  );
+  let output: Result<(bool, String, Option<Object>), Value> = rune::from_value(output)?;
+  if let Ok((result, message, audit)) = output {
+    println!(
+      "{}\n\t{}\t\t= {result}\n\t{}\t\t= {message}\n\t{}\t\t= {audit:?}",
+      "Result".green(),
+      "Correct".blue(),
+      "Message".blue(),
+      "Audit".blue()
+    );
+  } else {
+    println!(
+      "{}",
+      "Script returned early from '?' operators.".red().bold()
+    );
+  }
+  Ok(())
 }
 
 async fn environ(script: impl AsRef<Path>) -> anyhow::Result<()> {
-    let cwd = current_dir()?;
-    let mut vm = compile_source(script)?;
-    let mut user = Object::new();
-    user.insert(alloc::String::try_from("id")?, rune::to_value(3307)?)?;
-    user.insert(
-        alloc::String::try_from("account")?,
-        rune::to_value("p1ay3r")?,
-    )?;
-    user.insert(
-        alloc::String::try_from("institute_id")?,
-        rune::to_value(1106)?,
-    )?;
-    println!("{}\t\t= {user:?}", "User".blue());
-    let mut team = Object::new();
-    team.insert(alloc::String::try_from("id")?, rune::to_value(114514)?)?;
-    team.insert(alloc::String::try_from("name")?, rune::to_value("te4m")?)?;
-    team.insert(
-        alloc::String::try_from("institute_id")?,
-        rune::to_value(1106)?,
-    )?;
-    println!("{}\t\t= {team:?}", "Team".green());
+  let cwd = current_dir()?;
+  let mut vm = compile_source(script)?;
+  let mut user = Object::new();
+  user.insert(alloc::String::try_from("id")?, rune::to_value(3307)?)?;
+  user.insert(
+    alloc::String::try_from("account")?,
+    rune::to_value("p1ay3r")?,
+  )?;
+  user.insert(
+    alloc::String::try_from("institute_id")?,
+    rune::to_value(1106)?,
+  )?;
+  println!("{}\t\t= {user:?}", "User".blue());
+  let mut team = Object::new();
+  team.insert(alloc::String::try_from("id")?, rune::to_value(114514)?)?;
+  team.insert(alloc::String::try_from("name")?, rune::to_value("te4m")?)?;
+  team.insert(
+    alloc::String::try_from("institute_id")?,
+    rune::to_value(1106)?,
+  )?;
+  println!("{}\t\t= {team:?}", "Team".green());
 
-    println!(
-        "{}",
-        "---------------------------------------------".dimmed()
-    );
+  println!(
+    "{}",
+    "---------------------------------------------".dimmed()
+  );
 
-    let bucket = Bucket::try_new(&cwd)?;
-    let output = vm.call(["environ"], (bucket, user, team))?;
-    let object: Result<Object, Value> = rune::from_value(output)?;
-    if let Ok(object) = object {
-        let mut environ: HashMap<String, String> = HashMap::new();
-        for (key, value) in object.iter() {
-            environ.insert(key.to_string(), rune::from_value(value.clone())?);
-        }
-        println!("{}: {environ:?}", "Result".green());
-    } else {
-        println!("Error occured during env generation");
+  let bucket = Bucket::try_new(&cwd)?;
+  let output = vm.call(["environ"], (bucket, user, team))?;
+  let object: Result<Object, Value> = rune::from_value(output)?;
+  if let Ok(object) = object {
+    let mut environ: HashMap<String, String> = HashMap::new();
+    for (key, value) in object.iter() {
+      environ.insert(key.to_string(), rune::from_value(value.clone())?);
     }
-    Ok(())
+    println!("{}: {environ:?}", "Result".green());
+  } else {
+    println!("Error occured during env generation");
+  }
+  Ok(())
 }
--- src/modules/crypto.rs
+++ src/modules/crypto.rs
@@ -1,8 +1,8 @@
 use std::{f64, io};
 
 use ring::{
-    digest::{Context, SHA1_FOR_LEGACY_USE_ONLY, SHA256, SHA512},
-    rand::{self, SecureRandom},
+  digest::{Context, SHA1_FOR_LEGACY_USE_ONLY, SHA256, SHA512},
+  rand::{self, SecureRandom},
 };
 use rune::{Any, ContextError, Module};
 
@@ -16,23 +16,23 @@ use rune::{Any, ContextError, Module};
 /// ```
 #[rune::module(::ret2api::crypto)]
 pub fn module(_stdio: bool) -> Result<Module, ContextError> {
-    let mut module = Module::from_meta(self::module_meta)?;
-    module.ty::<Rng>()?;
-    module.function_meta(Rng::rand)?;
-    module.function_meta(Rng::rand_int)?;
-    module.ty::<Uuid>()?;
-    module.function_meta(Uuid::new)?;
-    module.ty::<Nanoid>()?;
-    module.function_meta(Nanoid::new)?;
-    module.ty::<Hash>()?;
-    module.function_meta(Hash::sha512sum)?;
-    module.function_meta(Hash::sha512sum_str)?;
-    module.function_meta(Hash::sha1sum)?;
-    module.function_meta(Hash::sha1sum_str)?;
-    module.function_meta(Hash::sha256sum)?;
-    module.function_meta(Hash::sha256sum_str)?;
+  let mut module = Module::from_meta(self::module_meta)?;
+  module.ty::<Rng>()?;
+  module.function_meta(Rng::rand)?;
+  module.function_meta(Rng::rand_int)?;
+  module.ty::<Uuid>()?;
+  module.function_meta(Uuid::new)?;
+  module.ty::<Nanoid>()?;
+  module.function_meta(Nanoid::new)?;
+  module.ty::<Hash>()?;
+  module.function_meta(Hash::sha512sum)?;
+  module.function_meta(Hash::sha512sum_str)?;
+  module.function_meta(Hash::sha1sum)?;
+  module.function_meta(Hash::sha1sum_str)?;
+  module.function_meta(Hash::sha256sum)?;
+  module.function_meta(Hash::sha256sum_str)?;
 
-    Ok(module)
+  Ok(module)
 }
 
 /// Pesudo-random numbers generator.
@@ -41,47 +41,49 @@ pub fn module(_stdio: bool) -> Result<Module, ContextError> {
 pub struct Rng;
 
 impl Rng {
-    /// get a float64 random numbers in range.
-    #[rune::function(path = Self::rand)]
-    pub fn rand(min: f64, max: f64) -> Result<f64, io::Error> {
-        if max <= min {
-            return Err(io::Error::other("max is less than min"));
-        }
-        let rng = rand::SystemRandom::new();
-        let mut buffer = [0u8; 8];
-        rng.fill(&mut buffer)
-            .map_err(|_| io::Error::other("generate random number failed"))?;
-        let mut result = f64::from_ne_bytes(buffer);
-        let step = max - min;
-        if result < min {
-            result += ((min - result) / step).ceil() * step;
-        }
-        if result > max {
-            result -= ((result - max) / step).ceil() * step;
-        }
-        Ok(result)
+  /// get a float64 random numbers in range.
+  #[rune::function(path = Self::rand)]
+  pub fn rand(min: f64, max: f64) -> Result<f64, io::Error> {
+    if max <= min {
+      return Err(io::Error::other("max is less than min"));
     }
+    let rng = rand::SystemRandom::new();
+    let mut buffer = [0u8; 8];
+    rng
+      .fill(&mut buffer)
+      .map_err(|_| io::Error::other("generate random number failed"))?;
+    let mut result = f64::from_ne_bytes(buffer);
+    let step = max - min;
+    if result < min {
+      result += ((min - result) / step).ceil() * step;
+    }
+    if result > max {
+      result -= ((result - max) / step).ceil() * step;
+    }
+    Ok(result)
+  }
 
-    /// get a int64 random numbers in range.
-    #[rune::function(path = Self::rand_int)]
-    pub fn rand_int(min: i64, max: i64) -> Result<i64, io::Error> {
-        if max <= min {
-            return Err(io::Error::other("max is less than min"));
-        }
-        let rng = rand::SystemRandom::new();
-        let mut buffer = [0u8; 8];
-        rng.fill(&mut buffer)
-            .map_err(|_| io::Error::other("generate random number failed"))?;
-        let mut result = i64::from_ne_bytes(buffer);
-        let step = max - min;
-        if result < min {
-            result += ((min - result) as f64 / (step as f64)).ceil() as i64 * step;
-        }
-        if result > max {
-            result -= ((result - max) as f64 / (step as f64)).ceil() as i64 * step;
-        }
-        Ok(result)
+  /// get a int64 random numbers in range.
+  #[rune::function(path = Self::rand_int)]
+  pub fn rand_int(min: i64, max: i64) -> Result<i64, io::Error> {
+    if max <= min {
+      return Err(io::Error::other("max is less than min"));
     }
+    let rng = rand::SystemRandom::new();
+    let mut buffer = [0u8; 8];
+    rng
+      .fill(&mut buffer)
+      .map_err(|_| io::Error::other("generate random number failed"))?;
+    let mut result = i64::from_ne_bytes(buffer);
+    let step = max - min;
+    if result < min {
+      result += ((min - result) as f64 / (step as f64)).ceil() as i64 * step;
+    }
+    if result > max {
+      result -= ((result - max) as f64 / (step as f64)).ceil() as i64 * step;
+    }
+    Ok(result)
+  }
 }
 
 /// Uuid generator.
@@ -92,11 +94,11 @@ impl Rng {
 pub struct Uuid;
 
 impl Uuid {
-    /// get a new uuid v4 string.
-    #[rune::function(path = Self::new)]
-    pub fn new() -> String {
-        uuid::Uuid::new_v4().to_string()
-    }
+  /// get a new uuid v4 string.
+  #[rune::function(path = Self::new)]
+  pub fn new() -> String {
+    uuid::Uuid::new_v4().to_string()
+  }
 }
 
 /// Nanoid generator.
@@ -105,11 +107,11 @@ impl Uuid {
 pub struct Nanoid;
 
 impl Nanoid {
-    /// get a new nanoid string.
-    #[rune::function(path = Self::new)]
-    pub fn new() -> String {
-        nanoid::nanoid!()
-    }
+  /// get a new nanoid string.
+  #[rune::function(path = Self::new)]
+  pub fn new() -> String {
+    nanoid::nanoid!()
+  }
 }
 
 /// Hash functions
@@ -120,51 +122,53 @@ impl Nanoid {
 pub struct Hash;
 
 impl Hash {
-    /// get sha256 sum hex string for message.
-    #[rune::function(path = Self::sha256sum)]
-    pub fn sha256sum(message: &[u8]) -> String {
-        let mut context = Context::new(&SHA256);
-        context.update(message);
-        hex::encode(context.finish().as_ref())
-    }
+  /// get sha256 sum hex string for message.
+  #[rune::function(path = Self::sha256sum)]
+  pub fn sha256sum(message: &[u8]) -> String {
+    let mut context = Context::new(&SHA256);
+    context.update(message);
+    hex::encode(context.finish().as_ref())
+  }
 
-    /// get sha256 sum hex string for message.
-    #[rune::function(path = Self::sha256sum_str)]
-    pub fn sha256sum_str(message: &str) -> String {
-        let mut context = Context::new(&SHA256);
-        context.update(message.as_bytes());
-        hex::encode(context.finish().as_ref())
-    }
+  /// get sha256 sum hex string for message.
+  #[rune::function(path = Self::sha256sum_str)]
+  pub fn sha256sum_str(message: &str) -> String {
+    let mut context = Context::new(&SHA256);
+    context.update(message.as_bytes());
+    hex::encode(context.finish().as_ref())
+  }
 
-    /// get sha512 sum hex string for message.
-    #[rune::function(path = Self::sha512sum)]
-    pub fn sha512sum(message: &[u8]) -> String {
-        let mut context = Context::new(&SHA512);
-        context.update(message);
-        hex::encode(context.finish().as_ref())
-    }
+  /// get sha512 sum hex string for message.
+  #[rune::function(path = Self::sha512sum)]
+  pub fn sha512sum(message: &[u8]) -> String {
+    let mut context = Context::new(&SHA512);
+    context.update(message);
+    hex::encode(context.finish().as_ref())
+  }
 
-    /// get sha512 sum hex string for message.
-    #[rune::function(path = Self::sha512sum_str)]
-    pub fn sha512sum_str(message: &str) -> String {
-        let mut context = Context::new(&SHA512);
-        context.update(message.as_bytes());
-        hex::encode(context.finish().as_ref())
-    }
+  /// get sha512 sum hex string for message.
+  #[rune::function(path = Self::sha512sum_str)]
+  pub fn sha512sum_str(message: &str) -> String {
+    let mut context = Context::new(&SHA512);
+    context.update(message.as_bytes());
+    hex::encode(context.finish().as_ref())
+  }
 
-    /// get sha1 sum hex string for message, for legacy use only, please use sha256 instead.
-    #[rune::function(path = Self::sha1sum)]
-    pub fn sha1sum(message: &[u8]) -> String {
-        let mut context = Context::new(&SHA1_FOR_LEGACY_USE_ONLY);
-        context.update(message);
-        hex::encode(context.finish().as_ref())
-    }
+  /// get sha1 sum hex string for message, for legacy use only, please use
+  /// sha256 instead.
+  #[rune::function(path = Self::sha1sum)]
+  pub fn sha1sum(message: &[u8]) -> String {
+    let mut context = Context::new(&SHA1_FOR_LEGACY_USE_ONLY);
+    context.update(message);
+    hex::encode(context.finish().as_ref())
+  }
 
-    /// get sha1 sum hex string for message, for legacy use only, please use sha256 instead.
-    #[rune::function(path = Self::sha1sum_str)]
-    pub fn sha1sum_str(message: &str) -> String {
-        let mut context = Context::new(&SHA1_FOR_LEGACY_USE_ONLY);
-        context.update(message.as_bytes());
-        hex::encode(context.finish().as_ref())
-    }
+  /// get sha1 sum hex string for message, for legacy use only, please use
+  /// sha256 instead.
+  #[rune::function(path = Self::sha1sum_str)]
+  pub fn sha1sum_str(message: &str) -> String {
+    let mut context = Context::new(&SHA1_FOR_LEGACY_USE_ONLY);
+    context.update(message.as_bytes());
+    hex::encode(context.finish().as_ref())
+  }
 }
